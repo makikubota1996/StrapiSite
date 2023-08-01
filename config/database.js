64fc05aff7ff71e5742e73ae69372cd94589d125
ichisaya-2 <maki.kubota@i-tem.info>
2023-08-01 13:14:38 +0900

    first commit

--- config/database.js
+++ config/database.js
@@ -1,11 +1,38 @@
 const path = require('path');
+const parse=require('pg-connection-string').parse;
+module.exports = ({ env }) => {
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+  if(env('NODE_ENV') ==='production"'){
+    const config =parse(process.env.DATABASE_URL);
+    return{
+      defaultConnection:'default',
+      connections:{
+        default:{
+          connetor:'bookshelf',
+          settings:{
+            client:'postgres',
+            host:config.host,
+            port:config.port,
+            database:config.database,
+            username:config.user,
+            password:config.password
+          },
+          options:{
+            ssl:false
+          },
+        }
+      }
+    };
+  }
+
+  return{
+    
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),},
+      useNullAsDefault: true,},
+  
+  };
+};
+
